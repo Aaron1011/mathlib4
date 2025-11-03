ac83bf3b46def07d49cffdd205cede92d1851892
Aaron Hill <aa1ronham@gmail.com>
2025-11-02 22:31:53 -0500

    Make more progress

--- Mathlib/Algebra/Group/Gromov/MatrixSubsum.lean
+++ Mathlib/Algebra/Group/Gromov/MatrixSubsum.lean
@@ -105,6 +105,85 @@ lemma subsums_unique {d: ℕ} (A: Matrix (Fin d) (Fin d) ℤ) (v: (Fin d) → �
           nth_rw 1 [p_eq] at h_sum
           rw [Finset.union_sdiff_distrib] at h_sum
           rw [Finset.sum_union] at h_sum
-          simp [n_q_diff] at h_sum
+          .
+            simp [n_q_diff] at h_sum
 
-          rw [add_comm] at h_sum
+            rw [add_comm] at h_sum
+            apply eq_add_neg_of_add_eq at h_sum
+            apply_fun norm at h_sum
+
+
+
+            have first_subset: q \ p ⊆ Finset.Ico N₀ (n + 1) := by
+              intro a ha
+              grind
+
+            have second_subset: (p \ {n}) \ q ⊆ Finset.Ico N₀ (n + 1) := by
+              intro a ha
+              grind
+
+            have a_pow_le: ‖(A ^ n).mulVec v‖ < ‖(A ^ n).mulVec v‖ := by
+              nth_rw 1 [h_sum]
+              rw [← Finset.sum_indicator_subset _ first_subset]
+              rw [← Finset.sum_indicator_subset _ second_subset]
+              rw [← sub_eq_add_neg]
+              rw [← Finset.sum_sub_distrib]
+              grw [norm_sum_le]
+              grw [Finset.sum_le_sum (g := (fun x ↦ ‖(A ^ x).mulVec v‖))]
+              .
+                sorry
+
+              . intro a ha
+                simp [Set.indicator]
+                split
+                . rename_i a_mem_q
+                  split
+                  . rename_i a_mem_other
+                    simp
+                  . simp
+                . split
+                  . simp
+                  . simp
+
+            simp at a_pow_le
+
+
+
+
+
+
+
+
+
+              -- have first_diff := Finset.sum_sdiff (f := fun n => (A^n).mulVec v) first_subset
+              -- simp at first_diff
+
+
+              -- rw [← Finset.sum_sdiff first_subset]
+
+
+
+
+              -- grw [abs_add_le]
+              -- simp
+              -- have first_subset: q \ p ⊆ ()
+              -- rw [Finset.sum_subset]
+              -- rw [neg_eq_neg_one_mul]
+              -- rw [Finset.mul_sum]
+              -- rw [← Finset.sum_union]
+
+              -- rw [← Finset.sum_sdiff_eq_sub]
+              -- . sorry
+              -- . intro a ha
+              --   simp
+              --   simp at ha
+              --   have first := ha.1.1
+              --   have second := ha.2
+              --   grind
+              -- sorry
+
+
+          .
+            rw [Finset.disjoint_iff_ne]
+            intro a ha b hb
+            grind
