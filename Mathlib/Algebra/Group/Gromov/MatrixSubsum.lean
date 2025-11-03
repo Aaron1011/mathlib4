d8adad03a48ce7101f40fcc58e086d8269940b6b
Aaron Hill <aa1ronham@gmail.com>
2025-11-02 21:41:49 -0500

    More stuff

--- Mathlib/Algebra/Group/Gromov/MatrixSubsum.lean
+++ Mathlib/Algebra/Group/Gromov/MatrixSubsum.lean
@@ -18,3 +18,80 @@ def poly_cancel {n: ℕ} (A: Matrix (Fin n) (Fin n) ℤ) (v: (Fin n) → ℤ) (p
     intro a ha b hb
     grind
 } : DerivedSets A v p q)
+
+
+lemma subsums_unique {d: ℕ} (A: Matrix (Fin d) (Fin d) ℤ) (v: (Fin d) → ℤ) (N₀ N: ℕ) (hn: N₀ ≤ N) (p q: Finset ℕ)
+  (hp: p ⊆ Finset.Ico N₀ N) (hq: q ⊆ Finset.Ico N₀ N) (hpq: p.sum (fun k => A^k • v) = q.sum (fun k => A^k • v)):
+    p = q := by
+
+  induction N, hn using Nat.le_induction generalizing p q with
+  | base =>
+    simp at hp
+    simp at hq
+    grind
+  | succ n hmn ih =>
+
+    by_cases n_both: n ∈ p ∧ n ∈ q
+    .
+      have p_eq: p = (p \ {n}) ∪ {n} := by
+        grind
+      have q_eq: q = (q \ {n}) ∪ {n} := by
+        grind
+      rw [p_eq, q_eq] at hpq
+      rw [Finset.sum_union] at hpq
+      rw [Finset.sum_union] at hpq
+      simp at hpq
+
+      have diff_eq := ih (p \ {n}) (q \ {n}) (by
+        intro c hc
+        simp at hc
+        have c_mem := hp hc.1
+        simp
+        simp at c_mem
+        omega
+      ) (by
+        intro c hc
+        simp at hc
+        have c_mem := hq hc.1
+        simp
+        simp at c_mem
+        omega
+      ) hpq
+      . grind
+      . simp
+      . simp
+    .
+      by_cases n_neither: n ∉ p ∧ n ∉ q
+      .
+        have p_eq: p = (p \ {n}) := by grind
+        have q_eq: q = (q \ {n}) := by grind
+        rw [p_eq, q_eq] at hpq
+        have diff_eq := ih (p \ {n}) (q \ {n}) (by
+          intro c hc
+          simp at hc
+          have c_mem := hp hc.1
+          simp
+          simp at c_mem
+          omega
+        ) (by
+          intro c hc
+          simp at hc
+          have c_mem := hq hc.1
+          simp
+          simp at c_mem
+          omega
+        ) hpq
+        grind
+      .
+        wlog n_mem_p: n ∈ p
+        .
+          have swapped := this A v N₀ N n hmn ih q p hq hp hpq.symm (by grind) (by grind) (by grind)
+          exact swapped.symm
+        .
+          rw [not_and_or] at n_neither
+          rw [not_and_or] at n_both
+
+          have n_mem_or: n ∈ p ∨ n ∈ q := by
+            grind
+
+          sorry
