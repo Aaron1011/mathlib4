bc89259c2d39e5ef034b81db43cdc1c29f3d8014
Aaron Hill <aa1ronham@gmail.com>
2025-11-02 20:26:05 -0500

    More work

--- Mathlib/Algebra/Group/Gromov/MatrixSubsum.lean
+++ Mathlib/Algebra/Group/Gromov/MatrixSubsum.lean
@@ -16,7 +16,7 @@ structure DerivedSets {n: ℕ} (A: Matrix (Fin n) (Fin n) ℤ) (v : (Fin n) → 
 --
 
 lemma poly_cancel {n: ℕ} (A: Matrix (Fin n) (Fin n) ℤ) (v: (Fin n) → ℤ) (p q : Finset ℕ) (hpq: p.sum (fun k => A^k • v) = q.sum (fun k => A^k • v)) (hp : ∃ a: ℕ, a ∈ p ∧ a ∉ q) : Nonempty (DerivedSets A v p q) := by
-
+  clear hpq
   induction p using Finset.induction_on generalizing q with
   | empty =>
     simp at hp
@@ -24,16 +24,16 @@ lemma poly_cancel {n: ℕ} (A: Matrix (Fin n) (Fin n) ℤ) (v: (Fin n) → ℤ) 
 
     by_cases a_mem_q: a ∈ q
     .
-      rw [Finset.sum_insert] at hpq
-      have orig_hpq := hpq
-      conv at hpq =>
-        rhs
-        arg 1
-        equals insert a (q \ {a}) =>
-          grind
-      simp [Finset.sum_insert] at hpq
+      -- rw [Finset.sum_insert] at hpq
+      -- have orig_hpq := hpq
+      -- conv at hpq =>
+      --   rhs
+      --   arg 1
+      --   equals insert a (q \ {a}) =>
+      --     grind
+      -- simp [Finset.sum_insert] at hpq
       obtain ⟨b, b_mem, b_not_mem⟩ := hp
-      obtain ⟨prev⟩ := ih (q \ {a}) (by exact hpq) (by grind)
+      obtain ⟨prev⟩ := ih (q \ {a}) (by grind)
       apply Exists.nonempty
 
       have s_not_mem_prev: a ∉ prev.p' := by
@@ -84,35 +84,66 @@ lemma poly_cancel {n: ℕ} (A: Matrix (Fin n) (Fin n) ℤ) (v: (Fin n) → ℤ) 
             grind
         }
     .
-      rw [Finset.sum_insert] at hpq
-      apply Exists.nonempty
-
-      obtain ⟨prev⟩ := ih (q) (by exact hpq) (by grind)
-      use {
-        p' := insert a prev.p',
-
-      }
-
-
-    by_cases s_empty: s = {}
-    .
-      simp [s_empty] at hpq
-      simp [s_empty]
-      simp at hp
-      simp [s_empty] at hp
-      grind
-    .
-      obtain ⟨b, hb, b_not_mem⟩ := hp
-
-      by_cases s_eq_q: s = q
+      --rw [Finset.sum_insert] at hpq
+      by_cases s_subset: s ⊆ q
       .
-        grind
-      .
-        by_cases q_extra: ∃ b ∈ q, b ∉ s
-        .
 
-        have prev := ih q (by grind) (by sorry)
-        exact {
-          p' := {}
+        obtain ⟨prev⟩ := ih (q) (by sorry)
+        apply Exists.nonempty
+
+        sorry
+      .
+        rw [Finset.not_subset] at s_subset
+        obtain ⟨b, hb, b_not_mem⟩ := hp
+        simp at hb
+
+        obtain ⟨prev⟩ := ih (q) (s_subset)
+        apply Exists.nonempty
+        use {
+          p' := prev.p',
+          q' := prev.q',
+          nontrivial := by
+            exact prev.nontrivial
+            --simp
+          supp_disj := by
+            exact prev.supp_disj
+            -- simp
+            -- have prev_disj := prev.supp_disj
+            -- have prev_q_derived := prev.q'_derived
+            -- grind
+          h_prime := by
+            exact prev.h_prime
+            -- have prev_h_prime := prev.h_prime
+            -- rw [Finset.sum_insert]
+            -- rw [prev_h_prime]
+          p'_derived := by
+            have prev_derived := prev.p'_derived
+            grind
+          q'_derived := by
+            have prev_derived := prev.q'_derived
+            grind
         }
-      sorry
+
+
+    -- by_cases s_empty: s = {}
+    -- .
+    --   simp [s_empty] at hpq
+    --   simp [s_empty]
+    --   simp at hp
+    --   simp [s_empty] at hp
+    --   grind
+    -- .
+    --   obtain ⟨b, hb, b_not_mem⟩ := hp
+
+    --   by_cases s_eq_q: s = q
+    --   .
+    --     grind
+    --   .
+    --     by_cases q_extra: ∃ b ∈ q, b ∉ s
+    --     .
+
+    --     have prev := ih q (by grind) (by sorry)
+    --     exact {
+    --       p' := {}
+    --     }
+    --   sorry
