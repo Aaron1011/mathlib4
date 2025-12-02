40c14062c65f25a0e7660bfb4ae4ac0cc201793f
Aaron Hill <aa1ronham@gmail.com>
2025-12-01 20:01:57 -0500

    Work on eigenvalue_adjoint

--- Mathlib/Analysis/Matrix/Unitary.lean
+++ Mathlib/Analysis/Matrix/Unitary.lean
@@ -9,6 +9,77 @@ structure IsoData {n: ℕ} {G: Subgroup (Matrix.unitaryGroup (Fin n) ℂ)} (g: G
   B: Subgroup (Matrix.unitaryGroup (Fin b) ℂ)
   iso: Subgroup.centralizer {g.val} ≃* A × B
 
+
+lemma star_normal_toContinuousLinearMap {A: Type*} [NormedAddCommGroup A] [InnerProductSpace ℂ A] [FiniteDimensional ℂ A]  (f: A →ₗ[ℂ] A):
+    IsStarNormal f ↔ IsStarNormal (LinearMap.toContinuousLinearMap f) := by
+
+  rw [isStarNormal_iff]
+  rw [isStarNormal_iff]
+  rw [ContinuousLinearMap.star_eq_adjoint]
+  rw [← LinearMap.adjoint_toContinuousLinearMap]
+  rw [LinearMap.star_eq_adjoint]
+  rw [commute_iff_eq]
+  rw [commute_iff_eq]
+  rw [LinearMap.ext_iff]
+  rw [ContinuousLinearMap.ext_iff]
+  simp
+
+-- https://math.stackexchange.com/a/4217008/367657
+lemma eigenvalue_adjoint {A: Type*} [NormedAddCommGroup A] [InnerProductSpace ℂ A] [FiniteDimensional ℂ A] (f: Module.End ℂ A) (k: ℂ) (v: A) (hf: IsStarNormal f):
+  f.HasEigenvector k v ↔ (star f).HasEigenvector (star k) v := by
+
+  have hf_cont := hf
+  rw [star_normal_toContinuousLinearMap] at hf_cont
+  have ker_adjoint := ContinuousLinearMap.IsStarNormal.ker_adjoint_eq_ker hf_cont
+  rw [Module.End.hasEigenvector_iff]
+  rw [Module.End.hasEigenvector_iff]
+  by_cases v_eq_zero: v = 0
+  . simp [v_eq_zero]
+  .
+    simp only [ne_eq, v_eq_zero, not_false_eq_true, and_true]
+    refine ⟨?_, ?_⟩
+    .
+      intro hf
+      rw [Module.End.eigenspace_def] at hf
+
+      rw [← LinearMap.ker_toContinuousLinearMap] at hf
+      rw [← ContinuousLinearMap.IsStarNormal.ker_adjoint_eq_ker] at hf
+      .
+        rw [← LinearMap.adjoint_toContinuousLinearMap] at hf
+        rw [LinearMap.ker_toContinuousLinearMap] at hf
+        rw [Module.End.eigenspace_def]
+        rw [LinearMap.star_eq_adjoint]
+        rw [map_sub] at hf
+        rw [LinearEquiv.map_smulₛₗ] at hf
+        conv at hf =>
+          pattern 1
+          equals LinearMap.id =>
+            rw [LinearMap.ext_iff]
+            simp
+        rw [LinearMap.adjoint_id] at hf
+        simpa using hf
+      .
+        rw [← star_normal_toContinuousLinearMap]
+        apply Commute.isStarNormal_sub
+        rw [commute_iff_eq]
+        simp
+    . sorry
+
+  -- TODO - figure out a way to re-use 'spectrum.map_star'
+  -- refine ⟨?_, ?_⟩
+  -- .
+  --   intro hf
+  --   have has_eigen: f.HasEigenvalue k := hf.hasUnifEigenvalue
+  --   rw [Module.End.hasEigenvalue_iff_mem_spectrum] at has_eigen
+  --   have foo := Set.mem_image_of_mem star has_eigen
+  --   have star_spec := spectrum.map_star f (R := ℂ)
+  --   simp only [Set.image_star] at foo
+  --   rw [← star_spec] at foo
+  --   rw [← Module.End.hasEigenvalue_iff_mem_spectrum] at foo
+
+
+  -- sorry
+
 lemma diag_of_eigenspace_span {A: Type*} [Nontrivial A] [AddCommGroup A] [Module ℂ A] (g: A →ₗ[ℂ] A) (k: ℂ) (hg: Module.End.eigenspace g k = ⊤):
   g = k • 1 := by
 
