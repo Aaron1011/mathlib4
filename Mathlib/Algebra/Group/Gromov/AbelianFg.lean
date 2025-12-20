28b3fabbb6c50b42a83dfcd99cd98decc5f85068
Aaron Hill <aa1ronham@gmail.com>
2025-12-20 15:00:50 -0500

    Add skeleton

--- Mathlib/Algebra/Group/Gromov/AbelianFg.lean
+++ Mathlib/Algebra/Group/Gromov/AbelianFg.lean
@@ -1,5 +1,26 @@
 import Mathlib
 
-lemma fg_subgroup_of_abelian {G : Type*} [CommGroup G] (hG : Group.FG G) (H : Subgroup G): H.FG :=
-
+lemma addgroup_fg_map {G G': Type*} [AddGroup G] [AddGroup G'] (H: AddSubgroup G) (h_fg: H.FG) (f: G →+ G'): (AddSubgroup.map f H).FG := by
   sorry
+
+lemma fg_subgroup_of_abelian {G : Type*} [AddCommGroup G] (hG : AddGroup.FG G) (H : AddSubgroup G): H.FG := by
+  have fg_top: (⊤ : AddSubgroup G).FG := by
+    rw [← AddGroup.fg_def]
+    infer_instance
+
+  rw [AddSubgroup.fg_iff_exists_fin_addMonoidHom] at fg_top
+  obtain ⟨n, f, hf⟩ := fg_top
+
+  let H' := AddSubgroup.comap f H
+
+  have H'_fg: H'.FG := by
+   sorry
+
+
+  let new_H := AddSubgroup.map f (AddSubgroup.comap f H)
+
+  have H_eq := AddSubgroup.map_comap_eq f H
+  simp [hf] at H_eq
+  rw [← H_eq]
+  apply addgroup_fg_map
+  apply H'_fg
