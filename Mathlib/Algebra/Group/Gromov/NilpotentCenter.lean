3c384380a7d77b13e41e9e096fc7f870a0659790
Aaron Hill <aa1ronham@gmail.com>
2026-02-09 20:34:50 -0500

    Work on nilpotency

--- Mathlib/Algebra/Group/Gromov/NilpotentCenter.lean
+++ Mathlib/Algebra/Group/Gromov/NilpotentCenter.lean
@@ -13,28 +13,133 @@ lemma lowerCentralSeries_congr {G H: Type*} [Group G] [Group H] [Group.IsNilpote
   have second := nilpotencyClass_le_of_surjective f.symm.toMonoidHom (by simp; exact MulEquiv.surjective f.symm)
   linarith
 
-lemma lowerCentralSeries_nilpotencyClass_succ {G: Type*} [Group G] [Group.IsNilpotent G] :
-    Group.nilpotencyClass (lowerCentralSeries G (Group.nilpotencyClass (G))) ≤ (Group.nilpotencyClass G) - 1 := by
+lemma lowerCentralSeries_lowerCentralSeries {G: Type*} [Group G] [Group.IsNilpotent G] {a b: ℕ}:
+    Subgroup.map (Subgroup.subtype _) (lowerCentralSeries (lowerCentralSeries G a) b) = lowerCentralSeries G (a + b) := by
 
-  match hn: (Group.nilpotencyClass G) with
-  | 0 =>
+  induction a with
+  | zero =>
+    simp
+    rw [le_antisymm_iff]
+    refine ⟨?_, ?_⟩
+    . grw [lowerCentralSeries_map_subtype_le]
+    .
+      induction b with
+      | zero =>
+        simp
+        ext a
+        simp
+      | succ n ih =>
+        rw [lowerCentralSeries_succ]
+        simp
+        intro x hx
+        simp at hx
+        simp
+        obtain ⟨p, hp, q, hq⟩ := hx
+        rw [mem_lowerCentralSeries_succ_iff]
+        apply Subgroup.mem_closure_of_mem
+        simp
+        use p
+        refine ⟨?_, ?_⟩
+        .
+          specialize ih hp
+          simpa using ih
+        .
+          use q
+          rw [← hq]
+          rfl
+  | succ n ih =>
+    ext a
+    refine ⟨?_, ?_⟩
+    .
+      intro ha
+      simp at ha
+      obtain ⟨a_mem, a_mem_nested⟩ := ha
+      rw [add_comm, ← add_assoc]
+      rw [mem_lowerCentralSeries_succ_iff]
+      apply Subgroup.mem_closure_of_mem
+      simp
+      
+
+
+
+
+lemma lowerCentralSeries_nilpotencyClass_succ {G: Type*} [Group G] [Group.IsNilpotent G] [Nontrivial G]  :
+    (Group.nilpotencyClass G) - 1 = (Group.nilpotencyClass (lowerCentralSeries G 1)) := by
+
+  induction hn: Group.nilpotencyClass G generalizing G with
+  | zero =>
     simp
     rw [nilpotencyClass_zero_iff_subsingleton] at hn
-    rw [nilpotencyClass_zero_iff_subsingleton]
+    rw [eq_comm, nilpotencyClass_zero_iff_subsingleton]
     infer_instance
-  | n + 1 =>
+  | succ k ih =>
     have foo := le_of_eq hn
     have bar := lowerCentralSeries_eq_bot_iff_nilpotencyClass_le.mpr foo
-    rw [bar]
-    simp
-    rw [← lowerCentralSeries_eq_bot_iff_nilpotencyClass_le]
-    match n with
-    | 0 =>
-      simp
-      exact Subgroup.eq_bot_of_subsingleton ⊤
-    | k + 1 =>
-      rw [lowerCentralSeries_succ]
-      simp
+    simp only [add_tsub_cancel_right]
+    rw [le_antisymm_iff]
+    refine ⟨?_, ?_⟩
+    .
+
+      sorry
+    .
+      rw [← lowerCentralSeries_eq_bot_iff_nilpotencyClass_le]
+
+      sorry
+
+
+      -- clear hn
+      -- induction n with
+      -- | zero =>
+      --   simp
+      --   rw [nilpotencyClass_zero_iff_subsingleton]
+      --   simp at foo
+      --   rw [← lowerCentralSeries_eq_bot_iff_nilpotencyClass_le] at foo
+      --   simp at foo
+      --   simp [foo]
+      --   exact Unique.instSubsingleton
+      -- | succ n ih =>
+      --   simp
+      --   rw [← lowerCentralSeries_eq_bot_iff_nilpotencyClass_le]
+      --   rw [lowerCentralSeries_succ]
+      --   rw [← le_bot_iff]
+      --   rw [Subgroup.closure_le]
+      --   intro x hx
+      --   simp
+      --   simp only [Set.mem_setOf_eq] at hx
+      --   obtain ⟨p, hp, q, _, hx⟩ := hx
+      --   rw [← hx]
+      --   rw [mul_eq_one_iff_inv_eq']
+      --   simp
+
+      --   have char: (commutator G).Characteristic := by infer_instance
+      --   rw [Subgroup.characteristic_iff_map_eq] at char
+      --   specialize char (MulAut.conj p)
+      --   simp at char
+
+      --   apply Subgroup.Normal.conj
+      --   rw [← mul_inv_eq_iff_eq_mul₀]
+      --   rw [← SetLike.coe_set_eq]
+      --   rw [Subgroup.coe_bot]
+
+      --   simp?
+
+
+
+
+    -- rw [nilpotencyClass_eq_quotient_center_plus_one]
+
+
+    -- rw [← nilpotencyClass_quotient_center]
+    -- rw [lowerCentralSeries_succ] at bar
+    -- rw [← lowerCentralSeries_eq_bot_iff_nilpotencyClass_le]
+    -- match n with
+    -- | 0 =>
+    --   simp
+    --   simp at hn
+    --   apply Subgroup.eq_bot_of_subsingleton
+    -- | k + 1 =>
+    --   rw [lowerCentralSeries_succ]
+    --   simp
 
 lemma lowerCentralSeries_nilpotency_sub {G: Type*} [Group G] [Group.IsNilpotent G] (n: ℕ):
   (Group.nilpotencyClass G) = (Group.nilpotencyClass (lowerCentralSeries G n)) + n := by
